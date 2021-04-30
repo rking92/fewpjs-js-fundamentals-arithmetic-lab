ec3ff84c15748de99899010538d0b7062d55d025
Robert <rking92@gmail.com>
2021-04-29 20:24:33 -0400

    commit tree 8463bd234c192a35fa5f6440995bc87a41cb8424

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+const num1 = 10
+const num2 =6.2
+const multiply = (num1*num2)
+multiply;
 
-// Write your code here
+const random = Math.ceil(Math.random())
+
+const num3 = 40
+const num4 = 9
+const mod = (num3 % num4)
+mod;
+
+const numberList = (2,4,5,7,14,20)
+const max = Math.max(numberList)
+max;
